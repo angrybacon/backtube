0d761229146895b0a49bf5390e9614438d2c8001
Mathieu Marques <mathieumarques78@gmail.com>
2018-02-18 20:47:14 +0100

    Add menu

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,19 +2,34 @@ import React from 'react';
 
 import AppBar from 'material-ui/AppBar';
 import Grid from 'material-ui/Grid';
-import IconButton from 'material-ui/IconButton';
+import Tabs, { Tab } from 'material-ui/Tabs';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import { Menu } from 'mdi-material-ui';
+import { FormatListBulleted, Magnify, Settings } from 'mdi-material-ui';
 
 class Header extends React.Component {
+
+  state = {currentTab: 'search'};
+
+  changeTab = (event, value) => {
+    this.setState({currentTab: value});
+  };
+
   render() {
     return (
       <AppBar>
         <Toolbar>
-          <Grid container alignItems="center" spacing={8}>
-            <Grid item><IconButton color="inherit"><Menu /></IconButton></Grid>
-            <Grid item><Typography color="inherit" variant="title">BackTube</Typography></Grid>
+          <Grid container alignItems="center" justify="space-between">
+            <Grid item>
+              <Typography color="inherit" variant="title">BackTube</Typography>
+            </Grid>
+            <Grid item>
+              <Tabs onChange={this.changeTab} value={this.state.currentTab}>
+                <Tab icon={<Magnify />} value="search"></Tab>
+                <Tab icon={<FormatListBulleted />} value="lists"></Tab>
+                <Tab icon={<Settings />} value="settings"></Tab>
+              </Tabs>
+            </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
