cfc9e9ca64aa7c58ef338a249c75fb7a6e6a622c
Mathieu Marques <mathieumarques78@gmail.com>
2018-02-24 13:19:21 +0100

    Mock buffering

--- src/scripts/player.progress.js
+++ src/scripts/player.progress.js
@@ -4,36 +4,34 @@ import { LinearProgress } from 'material-ui/Progress';
 
 class PlayerProgress extends React.Component {
 
-  state = {completed: 0};
+  state = {buffer: 10, completed: 0};
+
   timer = null;
-
-  componentDidMount() {
-    this.timer = setInterval(this.progress, 500);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
+  componentDidMount() { this.timer = setInterval(this.progress, 500); }
+  componentWillUnmount() { clearInterval(this.timer); }
 
   progress = () => {
     const { completed } = this.state;
-    if (completed === 100) {
-      this.setState({ completed: 0 });
+    if (completed > 100) {
+      this.setState({ buffer: 10, completed: 0 });
     }
     else {
-      const diff = Math.random() * 10;
-      this.setState({ completed: Math.min(completed + diff, 100) });
+      const a = Math.random() * 10;
+      const b = Math.random() * 10;
+      this.setState({ buffer: completed + a + b, completed: completed + a});
     }
   };
 
   render() {
+    const { buffer, completed } = this.state;
     return (
       <div style={{position: 'relative'}}>
         <LinearProgress
           color="secondary"
           style={{left: 0, position: 'absolute', right: 0, top: 0}}
-          value={this.state.completed}
-          variant="determinate"
+          value={completed}
+          valueBuffer={buffer}
+          variant="buffer"
           />
       </div>
     );
