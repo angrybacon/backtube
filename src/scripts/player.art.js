cbcecfc3243d088401e5308ae4bc86ff970676f4
Mathieu Marques <mathieumarques78@gmail.com>
2018-02-24 19:06:34 +0100

    Fix player layout (sort of)

--- src/scripts/player.art.js
+++ src/scripts/player.art.js
@@ -3,9 +3,7 @@ import React from 'react';
 class PlayerArt extends React.Component {
   render() {
     return (
-      <div className="player-art">
-        <img className="player-art-image" alt="" src="https://placecage.com/250/150" />
-      </div>
+      <img className="player-art-image" alt="" src="https://placecage.com/250/150" />
     );
   }
 }
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -3,6 +3,7 @@ import { withStyles } from 'material-ui/styles';
 
 import AppBar from 'material-ui/AppBar';
 import Grid from 'material-ui/Grid';
+import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
 import PlayerArt from './player.art';
@@ -21,26 +22,24 @@ class Player extends React.Component {
     const { classes } = this.props;
     return (
       <AppBar classes={{positionFixed: classes.positionFixed}} component="footer">
-        <Grid container spacing={0}>
-
-          <Grid item>
-            <PlayerArt />
-          </Grid>
-
-          <Grid item style={{flexGrow: 1}}>
-            <PlayerProgress/>
-            <Grid container alignItems="center" justify="space-between" style={{padding: 8}}>
-              <Grid item>
-                <Typography color="inherit">Track Name</Typography>
-                <Typography color="inherit" variant="caption">Album</Typography>
-                <Typography color="inherit" variant="caption">Artist</Typography>
-              </Grid>
-              <Grid item>
-                <PlayerControls />
-              </Grid>
+        <Toolbar disableGutters>
+          <Grid container alignItems="center" spacing={0}>
+            <Grid item className="player-art" style={{height: 64}}><PlayerArt /></Grid>
+            <Grid item style={{flexGrow: 1}}>
+              <PlayerProgress/>
+              <Toolbar>
+                <Grid container alignItems="center" justify="space-between">
+                  <Grid item>
+                    <Typography color="inherit">Track Name - Artist</Typography>
+                  </Grid>
+                  <Grid item>
+                    <PlayerControls />
+                  </Grid>
+                </Grid>
+              </Toolbar>
             </Grid>
           </Grid>
-        </Grid>
+        </Toolbar>
       </AppBar>
     );
   }
