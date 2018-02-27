ac044f1860cbef082f1ad951b43af48faad0846d
Mathieu Marques <mathieumarques78@gmail.com>
2018-02-27 17:53:03 +0100

    Clean up

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -5,7 +5,6 @@ import Grid from 'material-ui/Grid';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 
-import PlayerArt from './player.art';
 import PlayerControls from './player.controls';
 import PlayerProgress from './player.progress';
 
@@ -13,21 +12,14 @@ class Player extends React.Component {
   render() {
     return (
       <AppBar component="div" position="static">
-        <Toolbar disableGutters>
-          <Grid container alignItems="center" spacing={0}>
-            <Grid item className="player-art" style={{height: 64}}><PlayerArt /></Grid>
-            <Grid item style={{flexGrow: 1}}>
-              <PlayerProgress/>
-              <Toolbar>
-                <Grid container alignItems="center" justify="space-between">
-                  <Grid item>
-                    <Typography color="inherit">Track Name - Artist</Typography>
-                  </Grid>
-                  <Grid item>
-                    <PlayerControls />
-                  </Grid>
-                </Grid>
-              </Toolbar>
+        <PlayerProgress/>
+        <Toolbar>
+          <Grid container alignItems="center" justify="space-between">
+            <Grid item>
+              <Typography color="inherit">Track Name - Artist</Typography>
+            </Grid>
+            <Grid item>
+              <PlayerControls />
             </Grid>
           </Grid>
         </Toolbar>
